4193fc7528e113d08d1e47cea5329cd4f7ab8072
Mariya <mariyapavlova2001@mail.ru>
2021-04-06 15:26:49 +0400

    Добавила проекты и модели

---
+++ ClientApplicationBusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClientApplicationBusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ VetclinicStorage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VetclinicStorage
+{
+    public class Class1
+    {
+    }
+}
